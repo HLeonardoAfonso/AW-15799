32879fc9a3762781e3e0d69e06410906b334453f
HLeonardoAfonso <162568940+HLeonardoAfonso@users.noreply.github.com>
2024-04-29 13:09:12 +0100

    starting prisma

--- routes/privado.js
+++ routes/privado.js
@@ -2,9 +2,9 @@ const privadoRouter = require('express').Router();
 
 
 // Define uma rota para a página HTML
-privadoRouter.get('/gerirCarros', (req, res) => {
+privadoRouter.get('/', (req, res) => {
     // Envie o arquivo HTML como resposta para a solicitação HTTP
-    res.sendFile('/sites/ap2/webServiceJSON/templates/backOffice/tabelaCarros.html');
+    res.sendFile('templates/backOffice/tabelaCarros.html', { root: "." });
 });
 
 
